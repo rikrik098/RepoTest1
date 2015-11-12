9ebc5674d72728bb3f30ffb2b40d3bedc564075b
dsreddy <DhruthiReddy@Dhruthis-MacBook-Air.local>
2015-11-12 08:36:41 -0500

    Calculator
    New Calc Classes

---
+++ Calculator.swift
@@ -0,0 +1,13 @@
+//
+//  Calculator.swift
+//  Test1
+//
+//  Created by Dhruthi Reddy on 11/12/15.
+//  Copyright © 2015 user. All rights reserved.
+//
+
+import UIKit
+
+class Calculator: NSObject {
+
+}
